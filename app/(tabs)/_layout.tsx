000c40477675331b38b15ac366b97aa646f4a5fe
Artur Maurício <arturmauricioss@outlook.com>
2025-08-13 09:49:37 -0300

    alterando a tabbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -12,61 +12,66 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-            tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarLabelStyle: {
-          fontSize: 14,
-          textAlign: 'center',
-          marginBottom: 0,
-        },
-        tabBarStyle: {
-          height: 50,
-          paddingBottom: Platform.OS === 'android' ? 4 : 6,
-          backgroundColor: '#1e1e2f',
-          borderTopWidth: 0,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="draft"
-        options={{
-          title: 'Draft',
-        }}
-      />
-      <Tabs.Screen
-        name="heroes"
-        options={{
-          title: 'Heróis',
-        }}
-      />
-      <Tabs.Screen
-        name="meta"
-        options={{
-          title: 'Meta',
-        }}
-      />
-      <Tabs.Screen
-        name="players"
-        options={{
-          title: 'Jogadores',
-        }}
-      />
-      <Tabs.Screen
-        name="config"
-        options={{
-          title: 'Configurações',
-        }}
-      />
-      <Tabs.Screen
-        name="index"
-        options={{
-          href: null, // ❌ remove da tab bar
-        }}
-      />
-    </Tabs>
+   <Tabs
+  screenOptions={{
+    tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    headerShown: false,
+    tabBarBackground: TabBarBackground,
+    tabBarLabelStyle: {
+      fontSize: 14,
+      textAlign: 'center',
+      marginBottom: 0,
+    },
+    tabBarStyle: {
+      height: 50,
+      paddingBottom: Platform.OS === 'android' ? 4 : 6,
+      backgroundColor: '#1e1e2f',
+      borderTopWidth: 0,
+    },
+  }}
+>
+  <Tabs.Screen
+    name="draft"
+    options={{
+      title: 'Draft',
+      tabBarButton: HapticTab,
+    }}
+  />
+  <Tabs.Screen
+    name="heroes"
+    options={{
+      title: 'Heróis',
+      tabBarButton: HapticTab,
+    }}
+  />
+  <Tabs.Screen
+    name="meta"
+    options={{
+      title: 'Meta',
+      tabBarButton: HapticTab,
+    }}
+  />
+  <Tabs.Screen
+    name="players"
+    options={{
+      title: 'Jogadores',
+      tabBarButton: HapticTab,
+    }}
+  />
+  <Tabs.Screen
+    name="config"
+    options={{
+      title: 'Configurações',
+      tabBarButton: HapticTab,
+    }}
+  />
+  <Tabs.Screen
+    name="index"
+    options={{
+      href: null,
+    }}
+  />
+</Tabs>
+
   );
 }
